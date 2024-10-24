a28e6a8c12a67ec0edc85158d2ea78e9307a4e4f
Natiely <0001068756@senaimgaluno.com.br>
2024-10-24 15:21:07 -0300

    Adicionando exercicio 16

--- ConhendoFork/src/Ex16.java
+++ ConhendoFork/src/Ex16.java
@@ -1,10 +1,35 @@
 /*Contagem de Vogais: Peça uma frase ao usuário e conte quantas vogais 
  * (a, e, i, o, u) ela contém.
  */
+import java.util.Scanner;
+
 public class Ex16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+
+Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite uma frase: ");
+		String frase = scanner.nextLine();
+		
+		int contadoVogais = 0;
+		String vogais = "aeiouAEIOU";
+		
+
+        for (int i = 0; i < frase.length(); i++) {
+            if (vogais.indexOf(frase.charAt(i)) != -1) {
+                contadoVogais++;
+            }
+        }
+
+        
+        System.out.println("Número de vogais na frase: " + contadoVogais);
+        
+        
+        scanner.close();
+				
 
 	}
 
